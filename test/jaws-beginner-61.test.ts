aa623fa1771757f1f6fe1184c0150d71e2c84400
ren-yamanashi <pinokionigg@icloud.com>
2024-07-18 09:39:06 +0900

    Initial commit

---
+++ test/jaws-beginner-61.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as JawsBeginner61 from '../lib/jaws-beginner-61-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/jaws-beginner-61-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new JawsBeginner61.JawsBeginner61Stack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
